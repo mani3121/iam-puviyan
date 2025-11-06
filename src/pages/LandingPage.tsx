2d9512f05d309177d4d823eaad0671bd58b872fd
mani3121 <maniavudai10@gmail.com>
2025-11-06 22:38:36 +0530

    Update LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -152,7 +152,7 @@ const LandingPage = () => {
         </div>
         
         <div className="flex gap-5">
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-white/60 hover:text-primary hover:-translate-y-1 transition-all flex items-center justify-center">
+          <a href="https://x.com/PuviyanDigital" target="_blank" rel="noopener noreferrer" className="text-white/60 hover:text-primary hover:-translate-y-1 transition-all flex items-center justify-center">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
               <path d="M18.244 2.25h3.308l-7.227 8.26 8.502 11.24H16.17l-5.214-6.817L4.99 21.75H1.68l7.73-8.835L1.254 2.25H8.08l4.713 6.231zm-1.161 17.52h1.833L7.084 4.126H5.117z"/>
             </svg>
